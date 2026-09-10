8a5c166988a97d593d04a5b29a4d42c23dba5b11
damar24 <damarcahyono15@gmail.com>
2026-09-10 09:04:50 +0700

    Tugas Array Damar

---
+++ Perulangan.php
@@ -0,0 +1,12 @@
+<?php
+   $datasiswa = [
+    "data_nama_siswa_1" => "Damar",
+    "data_nisn_siswa_2" => "00097",
+    "data_Hobi_siswa_3" => "16",
+   ];
+    echo "data nama siswa 1", $datasiswa["data_nama_siswa_1"];
+    echo "<br/>";
+    echo "data nisn siswa 2", $datasiswa["data_nisn_siswa_2"];
+    echo "<br/>";
+    echo "data Hobi siswa 3", $datasiswa["data_Hobi_siswa_3"];
+    echo "<br/>";
